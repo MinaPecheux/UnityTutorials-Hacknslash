7d9df35ed94fd4c4e148ddb479b200635c030044
Mina Pêcheux <mina.pecheux@gmail.com>
2022-03-23 16:08:38 +0100

    feat(core): add tutorial 3 code

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,6 +8,8 @@ namespace Player
     public class PlayerController : MonoBehaviour
     {
         private CharacterController _controller;
+        [SerializeField] private Animator _animator;
+        private Transform _animatorTransform;
 
         #region Variables: Inputs
         private DefaultInputActions _inputActions;
@@ -17,12 +19,21 @@ namespace Player
         #region Variables: Movement
         private const float _MOVE_SPEED = 5f;
         private Vector2 _move;
+        private bool _running;
+        #endregion
+
+        #region Variables: Animation
+        private int _animRunningParamHash;
         #endregion
 
         private void Awake()
         {
             _inputActions = new DefaultInputActions();
             _controller = GetComponent<CharacterController>();
+            _animatorTransform = _animator.transform;
+
+            _running = false;
+            _animRunningParamHash = Animator.StringToHash("Running");
         }
 
         private void OnEnable()
@@ -39,10 +50,27 @@ namespace Player
         private void Update()
         {
             _move = _moveAction.ReadValue<Vector2>();
-            _controller.Move(
-                new Vector3(_move.x, 0f, _move.y) *
-                Time.deltaTime *
-                _MOVE_SPEED);
+            if (_move.sqrMagnitude > 0.01f)
+            {
+                if (!_running)
+                {
+                    _running = true;
+                    _animator.SetBool(_animRunningParamHash, true);
+                }
+
+                Vector3 v = new Vector3(_move.x, 0f, _move.y);
+                _animatorTransform.rotation =
+                    Quaternion.LookRotation(-v, Vector3.up);
+                _controller.Move(
+                    v *
+                    Time.deltaTime *
+                    _MOVE_SPEED);
+            }
+            else if (_running)
+            {
+                _running = false;
+                _animator.SetBool(_animRunningParamHash, false);
+            }
         }
     }
 
