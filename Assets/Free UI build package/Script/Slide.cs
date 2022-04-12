b4a76933114f3760401127ff6cf45baf15e40379
Mina Pêcheux <mina.pecheux@gmail.com>
2022-04-12 18:21:08 +0200

    chore(pkg): add Free UI Pack lib

---
+++ Assets/Free UI build package/Script/Slide.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slide : MonoBehaviour {
+	Image Filler;
+	public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		Filler = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Filler.fillAmount = slider.value;
+	}
+}
---
+++ Assets/Free UI build package/Script/Togglescript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Togglescript : MonoBehaviour {
+
+    Toggle toggle;
+
+    private void Start()
+    {
+        toggle = GetComponent<Toggle>();
+    }
+
+    public GameObject Slider;
+
+
+    private void Update()
+    {
+        if (toggle.isOn)
+        {
+            Slider.SetActive(false);
+        }
+        else
+        {
+            Slider.SetActive(true);
+        }
+    }
+}
